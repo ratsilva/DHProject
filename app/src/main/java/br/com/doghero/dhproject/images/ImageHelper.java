e4bdc8c6a89612377ed124f79df4d2356b79efbc
Ricardo Silva <ricardosilva@macbook-pro-de-ricardo.local>
2019-02-04 14:56:31 -0200

    Criando activity HeroesActivity para geração da funcionalidade de Meus Heróis

--- app/src/main/java/br/com/doghero/dhproject/images/ImageHelper.java
+++ app/src/main/java/br/com/doghero/dhproject/images/ImageHelper.java
@@ -3,6 +3,8 @@ package br.com.doghero.dhproject.images;
 import android.content.Context;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 public class ImageHelper {
     public static void loadImage(Context context, String imageUrl, int placeHolderResourceId, ImageView imageView) {
         Picasso.with(context)
